41a21cd8de26fa5fb3988e60f798687c683d7f74
Brian Eazer <brianeazer2015@gmail.com>
2018-10-11 20:21:25 -0400

    Second commit. Added running total score

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -15,7 +15,8 @@ public class DiceRoll {
 		if (sc.hasNextInt()) {
 			diceSides = sc.nextInt();
 			int counter = 1;
-			askToPlayFirstTime(diceSides, counter);
+			int total = 0;
+			askToPlayFirstTime(diceSides, counter, total);
 		}
 		else {
 			System.out.println("Oops");
@@ -24,34 +25,38 @@ public class DiceRoll {
 	}
 
 
-	private static void askToPlayFirstTime(int diceSides, int counter) {
+	private static void askToPlayFirstTime(int diceSides, int counter, int total) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Would you like to roll the dice? y/n?");
 		if (sc.next().equals("y")) {
-			System.out.println("For roll 1 your score is " + generateRandomDieRoll(diceSides, counter));
+			int roll = generateRandomDieRoll(diceSides, counter, total);
+			total = calcTotal(roll, total);
+			System.out.println("For roll 1 your score is " + roll);
 		}
 		else {
 			System.out.println(".");
 		}
-		askToPlayAgain(diceSides, counter);
+		askToPlayAgain(diceSides, counter, total);
 	}
 
-	private static void askToPlayAgain(int diceSides, int counter) {
+	private static void askToPlayAgain(int diceSides, int counter, int total) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Would you like to roll those dice again? y/n?");
 		counter++;
 		if (sc.next().equals("y")) {
-			System.out.println("OK, now for roll " + counter + " your score is " + generateRandomDieRoll(diceSides, counter));
-			askToPlayAgain(diceSides, counter);
+			int roll = generateRandomDieRoll(diceSides, counter, total);
+			total = calcTotal(roll, total);
+			System.out.println("OK, now for roll " + counter + " your score is " + roll + " and your total score is " + total + "");
+			askToPlayAgain(diceSides, counter, total);
 		}
 		else {
 			System.out.println("Well then.");
 		}
 	}
 
-	private static int generateRandomDieRoll(int sides, int counter) {
+	private static int generateRandomDieRoll(int sides, int counter, int total) {
 		// TODO Auto-generated method stub
 		int dice1 = (int) (Math.random() * sides) + 1;
 		int dice2 = (int) (Math.random() * sides) + 1;
@@ -66,6 +71,10 @@ public class DiceRoll {
 			System.out.print("OUCH. Craps! ");
 		}
 		return sum;
+	}
+	private static int calcTotal(int score, int total) {
+		total += score;
+		return total;
 	}
 
 }
